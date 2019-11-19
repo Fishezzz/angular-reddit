cce2acf49de78ae2ee29f9b52d8b75e453fb6dc2
Fishezzz <33294032+Fishezzz@users.noreply.github.com>
2019-11-19 09:23:20 +0100

    #5

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
-  selector: 'app-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+    selector: 'app-article',
+    templateUrl: './article.component.html',
+    styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+    @HostBinding('attr.class') cssClass = 'row';
+    votes: number;
+    title: string;
+    link: string;
 
-  constructor() { }
+    constructor() {
+        this.title = 'Angular2';
+        this.link = 'http://angular.io';
+        this.votes = 10;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    voteUp() {
+        this.votes += 1;
+    }
+
+    voteDown() {
+        this.votes -= 1;
+    }
 }
